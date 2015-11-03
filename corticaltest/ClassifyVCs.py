fb60b24822392c04e5815452c26e8a116fed1764
avi yashchin <avi.yashchin@gmail.com>
2015-11-03 17:30:55 +0000

    adding files

---
+++ corticaltest/ClassifyVCs.py
@@ -0,0 +1,73 @@
+import cortipy
+import os
+import mysql.connector as msc
+import time
+
+# Init API client
+apiKey = os.environ.get('CORTICAL_API_KEY')
+client = cortipy.CorticalClient(apiKey)
+
+
+MYSQL_GSJ_USER = 'root'
+MYSQL_GSJ_PASSWORD = 'nycdsa1!'
+MYSQL_GSJ_HOST = '173.194.225.231'
+MYSQL_GSJ_DB = 'test'
+
+config = {
+   'user': 'root',
+   'password': 'uLFZ2WoB',
+   'host': '130.211.154.93',
+   'database': 'test',
+   'charset': 'utf8'
+}
+
+#Need to put this into an infinite loop
+while 1:
+    con = msc.connect(**config)
+    cur = con.cursor()
+    #con.autocommit(True)
+    time.sleep(1)
+
+    cur.execute("select siteurl, text from vctest where cortical_io is null limit 1")
+    test = cur.fetchall()
+
+    siteurl = str(test[0][0])
+    text = str(test[0][1])
+    #programmingCategory = client.createClassification(categoryName, pos, neg)
+
+    if(text == ''):
+        continue
+
+    termKeyWords = client.extractKeywords(text)
+    termBitmap = client.getTextBitmap(text)['fingerprint']['positions']
+
+    MySqlKeyWordDat = (','.join(termKeyWords), siteurl)
+    MySqlBitMapDat = (str(termBitmap), siteurl)
+
+    MySqlKeyWordDatQ = """UPDATE vctest SET cortical_io_keywords = %s WHERE siteurl = %s"""
+    MySqlBitMapDatQ = """UPDATE vctest SET cortical_io = %s WHERE siteurl = %s"""
+
+    #upload keywords and bitmap to database
+    cur.execute(MySqlKeyWordDatQ, MySqlKeyWordDat)
+    cur.execute(MySqlBitMapDatQ, MySqlBitMapDat)
+    con.commit()
+
+    con.close()
+
+#bitmapTerms = client.bitmapToTerms(termBitmap['fingerprint'])
+
+
+
+
+
+
+######  sample code bleow.
+# Evaluate how close a new term is to the category.
+#termBitmap = client.getBitmap("Python")['fingerprint']['positions']
+#distances = client.compare(termBitmap, programmingCategory['positions'])
+#print distances['euclideanDistance']
+
+# Try a block of text.
+#textBitmap = client.getTextBitmap("The Zen of Python >>>import this")['fingerprint']['positions']
+#distances = client.compare(textBitmap, programmingCategory['positions'])
+#print distances['euclideanDistance']
