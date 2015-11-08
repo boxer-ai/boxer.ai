8c23250f7911f75ae4410d37eb4adbe3fdaceb40
Avi Yashchin <avi.yashchin@gmail.com>
2015-11-08 00:13:23 -0500

    adding NLP

---
+++ NLPDaemon.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+#
+#
+#
+echo "usage = ./NLPDaemon.sh vctest4 (VC scraper) -or- ./NLPDaemon.sh crunchbase_startups (startup capital scraper)"
+
+  source ~/.bashrc
+
+  while true; do
+    `nohup python NLPDaemon.py $1`
+    sleep 60
+  done
